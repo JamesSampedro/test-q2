810065b96846d6a78f93f7b259716ab6349fbed7
MATHEW CLINT BRAZAL <1815-18@itmlyceumalabang.onmicrosoft.com>
2020-12-08 02:28:51 +0800

    Add project files.

---
+++ Unique/Data/IdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unique.Models;
+
+namespace Unique.Data
+{
+    public class IdentityDbContext : DbContext
+    {
+        public IdentityDbContext( DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<ApplicationUser> applicationUsers { get; set; }
+
+    }
+}
